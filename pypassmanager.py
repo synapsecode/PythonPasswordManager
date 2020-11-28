dff72bbdaf202cb3ffde3d2a08a4ef41fa641e87
Manas Hejmadi | Developer <67999871+synapsecode@users.noreply.github.com>
2020-11-29 00:56:29 +0530

    Copy2Clipboard & Added Keygen

--- pypassmanager.py
+++ pypassmanager.py
@@ -1,25 +1,23 @@
 import os
 import json 
 from cryptography.fernet import Fernet
+import cryptography
 import sys
 import argparse
 from prettytable import PrettyTable
+import pyperclip as pc
 
+#Constants
 base = os.path.dirname(os.path.abspath(__file__))
 config = json.loads(open(os.path.join(base, "config.json"), "rb").read() or "{}")
-
-
-#Getting the CryptoKey from the Environment Variables
-cryptokey = os.environ.get('ppmkey')
+#Environment Variables
+ppmkey = os.environ.get('ppmkey')
+ppmcryptokey = os.environ.get('ppmcryptokey')
 
 """
 TO ADD:
-1. Support to store cryptokey in env
-
-
-DONE
-1. Support for Environment Variable key
 """
+
 
 def ScanASCII(iterable):
 	a = 0
@@ -52,18 +50,20 @@ def ceasar_cipher(s, shift, decrypt=False):
 
 
 def crypto(S, dec=False):
-	# print("CRP:", S, dec)
-	# cryptokey = config['authkey']
-	K = config['authkey']
-	F = Fernet(K)
-	#Encryption or Decryption
-	S = S if dec else bytes(S, encoding='ascii')
-	return F.decrypt(S) if dec else F.encrypt(S)
+	try:
+		F = Fernet(ppmcryptokey)
+		S = S if dec else bytes(S, encoding='ascii')
+		return F.decrypt(S) if dec else F.encrypt(S)
+	except cryptography.fernet.InvalidToken:
+		print("Invalid Cryptographic Token")
+		exit()
+	except Exception as e:
+		print("Unknown Error:", e)
+	
+	
 
 def encrypt(s, k):
 	enck = ceasar_cipher(k, len(s)) #Ceasar Encrypting key
-	# asciisum = sum([ord(x) for x in enck]) #Ascii sum of Encrypted Key
-
 	asciisum = sum(ScanASCII(enck)) #New Scanning Sun
 	encs = ceasar_cipher(s, asciisum) #Ceasar Encrypting of String
 	cen = crypto(encs) #Cryptographic Encoding
@@ -73,7 +73,6 @@ def decrypt(s, k):
 	cdec = crypto(s, dec=True) #Cryptographic Decoding
 	S = cdec.decode('ascii') #Bytes -> String
 	deck = ceasar_cipher(k, len(S)) #Ceasar Encrypting key
-	# asciisum = sum([ord(x) for x in deck]) #Ascii sum of Encrypted Key
 	asciisum = sum(ScanASCII(deck)) #New Scanning Sun
 	decs = ceasar_cipher(S, asciisum, decrypt=True) #Ceasar Decrypting of String
 	return decs
@@ -82,21 +81,27 @@ if __name__ == '__main__':
 	#Arguement Parser Stuff
 	parser = argparse.ArgumentParser()
 	parser.add_argument('command', help="PPM Command", type= str)
-	parser.add_argument('--cryptokey', '-k', help="Cryptographic Key", type= str)
+	parser.add_argument('--ppmkey', '-k', help="PPM Key", type= str)
 	parser.add_argument('--object', '-o', help="Payload Object", type= str)
 	parser.add_argument('--name', '-n', help="Password Field Name", type= str)
 	parser.add_argument('--password', '-p', help="Password", type= str)
 	parser.add_argument('--ppxpath', '-ppxp', help="PPXPath", type= str)
 	args = parser.parse_args()
 
-	def check_cryptokey():
-		global cryptokey
-		if(args.cryptokey):
-			cryptokey = args.cryptokey
+	def validate():
+		global ppmkey
+		if(args.ppmkey):
+			ppmkey = args.ppmkey
 
-		if(not args.cryptokey and not cryptokey):
+		if(not args.ppmkey and not ppmkey):
 			print("Please Include the Global CryptoKey with the -k flag or add it to your Environment Variables as 'ppmkey'")
 			exit()
+
+		global ppmcryptokey
+		if(not ppmcryptokey):
+			print("'ppmcryptokey' not found in your environment, Please use 'ppm keygen' to generate a ppmkey and add it to your environment")
+			exit()
+
 
 	def get_enc_path(p):
 		return base if p == "base" else p
@@ -113,34 +118,36 @@ if __name__ == '__main__':
 	D = json.loads(open(os.path.join(get_enc_path(config['ppxpath']), "encdata.ppx")).read() or "{}")
 
 	if(args.command == "get"):
-		check_cryptokey()
+		validate()
 		X = D.get(args.name)
 		if(X):
-			print(decrypt(bytes(X, encoding='ascii'), cryptokey))
+			pw = decrypt(bytes(X, encoding='ascii'), ppmkey)
+			print(pw, "(copied to clipboard)")
+			pc.copy(pw)
 		else:
 			print("No Such Password Identifier Could be Found")
 
 	elif(args.command == "add" or args.command == "update"):
-		check_cryptokey()
+		validate()
 		if(args.object):
-			D[args.name] = encrypt(args.object, cryptokey)[1].decode('ascii')
+			D[args.name] = encrypt(args.object, ppmkey)[1].decode('ascii')
 		elif(args.password):
-			D[args.name] = encrypt(args.password, cryptokey)[1].decode('ascii')
+			D[args.name] = encrypt(args.password, ppmkey)[1].decode('ascii')
 		else:
 			print("Please Include New Password using -o or -p Flags")
 
 		save_enc()
 
 	elif(args.command == 'del'):
-		check_cryptokey()
+		validate()
 		D.pop(args.name) #!Handle KeyError
 		save_enc()
 
 	elif(args.command == 'list'):
-		check_cryptokey()
+		validate()
 		PTable = PrettyTable(['Name', 'Password'])
 		for K, V in D.items():
-			PTable.add_row([K,  decrypt(bytes(V, encoding='ascii'), cryptokey)])
+			PTable.add_row([K,  decrypt(bytes(V, encoding='ascii'), ppmkey)])
 		print(PTable)
 
 	elif(args.command == 'clear'):
@@ -164,16 +171,19 @@ if __name__ == '__main__':
 			#Update Configuration
 			save_config()
 
+	elif(args.command == 'keygen'):
+		if(len(D) == 0):
+			key = Fernet.generate_key().decode('ascii')
+			print("Authentication Key: ", key, "(copied to clipboard)")
+			pc.copy(key)
+			print("Add this key to Environment Variable: 'ppmcryptokey'")
+		else:
+			print("Cannot Generate new Cryptographic Keys if you already have encrypted passwords")
+
 
 	#MINOR COMMANDS
 	else:
 		if(args.command == "cppxp"):
 			print("Current Location of .ppx File:", get_enc_path(config['ppxpath']))
-		# elif(args.command == "cryptoenv"):
-		# 	config['crypto_env'] = True
-		# 	#Add to ENV
-		# 	os.environ['cryptoenv'] = "X"
-		# 	print("X", os.environ['cryptoenv'])
-		# 	save_config()
 		else:
 			print("Invalid Command")
